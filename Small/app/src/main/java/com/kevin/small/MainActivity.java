d2dc86bc1a9ed2d8bd15faa6ee14a484903d1a07
ZhangKuixun <ZhangKuixun@gmail.com>
2019-09-21 20:45:01 +0800

    集成Small成功，输出“Hello Small!”。重新创建一个应用模块 Phone & Tablet Module

--- Small/app/src/main/java/com/kevin/small/MainActivity.java
+++ Small/app/src/main/java/com/kevin/small/MainActivity.java
@@ -1,7 +1,9 @@
 package com.kevin.small;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import net.wequick.small.Small;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Small.setUp(this, new Small.OnCompleteListener() {//加载插件是一个耗时操作
+            @Override
+            public void onComplete() {
+                //参数1 要启动的插件。参数2 当前上下文
+                Small.openUri("main", MainActivity.this);
+            }
+        });
+
     }
 }
